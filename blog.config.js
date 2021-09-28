8380994ec9b064e0039e07b7c52f84c259092874
wtser <wangtieshou@gmail.com>
2021-09-28 08:09:16 +0800

    Change domain url

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: "王铁手的博客",
+  title: "漫读",
   author: "wtser",
   email: "wtser@qq.com",
-  link: "https://wtser.com",
+  link: "https://read.wtser.com",
   description: "Find a faster way to fail, recover and try again.",
   lang: "cn-ZH",
   appearance: "auto", // ['light', 'dark', 'auto'],
